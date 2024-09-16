85a1278f951fcc209ee531c6c86e04749d9e9c2b
IlluminateDreamer <kabeercybersec@gmail.com>
2024-09-16 20:40:26 +0500

    merge-two-sorted-lists

---
+++ merge-two-sorted-lists.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode;
+        ListNode* newList = dummy;
+        while(list1 && list2) {
+            if(list1->val <= list2->val) {
+                newList->next = list1;
+                list1 = list1->next;
+            } else {
+                newList->next = list2;
+                list2 = list2->next;
+            }
+            newList = newList->next;
+        }
+        if (list1) newList->next = list1;
+        else newList->next = list2;
+        return dummy->next;
+    }
+};
